7506feb784984900174ce12a9f344af3be5282ae
agustinsoler12 <agustinsoler2008@gmail.com>
2026-04-24 05:18:22 +0200

    Add files via upload

---
+++ procesar.php
@@ -0,0 +1,10 @@
+<?php
+
+// Recibimos los datos del formulario usando el array $_POST
+$nombre = htmlspecialchars($_POST['nombre']);
+$edad = (int) $_POST['edad'];
+
+// Imprimimos la respuesta dinamica
+echo 'Hola, '. $nombre . '.';
+echo 'Tienes '. $edad . 'años.';
+?>
---
+++ script.js
@@ -0,0 +1,25 @@
+const redColor = document.querySelector('.red-btn');
+const grayColor = document.querySelector('.gray-btn');
+
+const cartButton = document.getElementById('cartButton');
+const imageCard = document.querySelector('.image-card');
+
+
+redColor.addEventListener('click', function(){
+
+cartButton.style.backgroundColor='red';
+
+imageCard.style.backgroundImage=
+'url("red-benz.webp")';
+
+});
+
+
+grayColor.addEventListener('click', function(){
+
+cartButton.style.backgroundColor='#111';
+
+imageCard.style.backgroundImage=
+'url("gray-benz.webp")';
+
+});
